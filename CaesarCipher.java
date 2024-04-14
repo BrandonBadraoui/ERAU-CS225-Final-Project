a7b096464eb811295754ae94e02a6c1c1f84483b
BrandonBadraoui <166546260+BrandonBadraoui@users.noreply.github.com>
2024-04-14 15:42:47 -0400

    CaesarCipher.java
    P6's Iteration of the CaesarCipher Class.

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -5,26 +5,27 @@
  * Created: 3/25/2024
  * Modified: 4/5/2024
  * Purpose: This is the computation for the caesar cipher.
- *
- *
+ * <p>
  * Attributes:
  * -ccCipherInt: int[]
- *
- *
+ * <p>
  * Methods:
- * +runCipher(int[],int): int[]
+ * +runCipher(int[]): int[]
  * +getCcCipherInt(): int[]
  ***********************************/
 public class CaesarCipher extends EmptyCipher {
     private int[] ccCipherInt;
+
     @Override
     public int[] runCipher(int[] userInAsNum) {
         ccCipherInt = new int[userInAsNum.length];
         for (int i = 0; i < userInAsNum.length; i++) {
-            ccCipherInt[i] = (userInAsNum[i] + 3) % 37;
+            ccCipherInt[i] = (userInAsNum[i] + 3) % 36;
         }
         return ccCipherInt;
     }
 
-    public int[] getCcCipherInt() {return ccCipherInt;}
+    public int[] getCcCipherInt() {
+        return ccCipherInt;
+    }
 }
