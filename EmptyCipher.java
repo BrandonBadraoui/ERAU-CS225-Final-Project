1ac3c336fc6cec56588c98bf69dd983a795209b7
BrandonBadraoui <166546260+BrandonBadraoui@users.noreply.github.com>
2024-04-22 09:38:56 -0400

    EmptyCipher.java

--- EmptyCipher.java
+++ EmptyCipher.java
@@ -3,7 +3,7 @@
  * Author: Brandon Badraoui
  * Collaborators:
  * Created: 3/25/2024
- * Modified: 4/5/2024
+ * Modified: 4/14/2024
  * Purpose: This is the parent class for all the types of ciphers.
  * <p>
  * Attributes:
